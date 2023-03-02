c5520537b23d356c1323656048b6e7a39a66caa2
rakib <phi.rakib@gmail.com>
2023-03-02 16:22:14 +0600

    added web routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PostController::class)->group(function() {
+    Route::get('posts', 'index')->name('posts.index');
+    Route::get('posts/create', 'create')->name('posts.create');
+    Route::post('posts', 'store')->name('posts.store');
 });
